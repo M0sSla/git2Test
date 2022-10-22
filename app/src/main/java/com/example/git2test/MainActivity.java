e8cc52d6b8b536c8714a0931ed71724467bb5e81
MosSla <mossla@inbox.ru>
2022-10-22 17:28:50 +0300

    secc

--- app/src/main/java/com/example/git2test/MainActivity.java
+++ app/src/main/java/com/example/git2test/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hi yu wazzup!");
+        System.out.println("Bye nig!");
     }
 }
